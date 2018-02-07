d5b2aaa996a2745b8dd2ade48206423aa14e633e
EstrellaBF <stern.von.nord>
2018-02-07 17:45:03 -0500

    terminando trazado

--- js/app.js
+++ js/app.js
@@ -22,21 +22,69 @@ if (navigator.geolocation) {
       });
       // fin api googe maps
 
-    }
-    function error() {
-      showMap.innerHTML = '<p>No se pudo obtener tu ubicación.</p>';
-    }
-    navigator.geolocation.getCurrentPosition(localization, error);
 
     // Autocompletado
     let startLocation = document.getElementById('start-location');
     let endLocation = document.getElementById('end-location');
     new google.maps.places.Autocomplete(startLocation);
     new google.maps.places.Autocomplete(endLocation);
-  }
-  google.maps.event.addDomListener(window, 'load', activatePlacesSearch);
 
-  
+    // Calculando ruta
+    let directionsService = new google.maps.DirectionsService;
+    let directionsDisplay = new google.maps.DirectionsRenderer;
+    let calculateAndDisplayRoute = function (directionsService, directionsDisplay) {
+      directionsService.route({
+        origin: startLocation.value,
+        destination: endLocation.value,
+        travelMode: 'DRIVING'
+      }, function(response, status){
+        if(status==='OK'){
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Tu ruta no se encuentra disponible');
+        }
+      });
+    };
+    directionsDisplay.setMap(map);
+    let traceRoute = ()=>{
+      calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('mark-route').addEventListener('click', traceRoute);
+
+
+
+    }
+    function error() {
+      showMap.innerHTML = '<p>No se pudo obtener tu ubicación.</p>';
+    }
+    navigator.geolocation.getCurrentPosition(localization, error);
+
+
+    // // Función para trazar ruta
+    // let calculateAndDisplayRoute = function(directionsService, directionsDisplay){
+    //   directionsService.route({
+    //     origin: inputPartida.value,
+    //     destination: inputDestino.value,
+    //     travelMode: 'DRIVING'
+    //   }, function(response, status){
+    //     if(status==='OK'){
+    //       directionsDisplay.setDirections(response);    // Trazando ruta
+    //       directionsDisplay.setMap(map);
+    //       var setRoute = function(){
+    //         calculateAndDisplayRoute(directionsService, directionsDisplay);
+    //       };
+    //       document.getElementById('mark-route').addEventListener('click', setRoute);
+    //     } else {
+    //       window.alert('Tu ruta no se encuentra disponible');
+    //     }
+    //   });
+    // };
+
+
+  }
+  // google.maps.event.addDomListener(window, 'load', activatePlacesSearch);
+
+
 
 }
 else {
