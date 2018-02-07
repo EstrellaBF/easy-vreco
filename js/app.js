13f9902907eb8301390d0795579e35018fab2dc6
EstrellaBF <stern.von.nord>
2018-02-07 17:16:54 -0500

    completando mapa y autocompletado

--- js/app.js
+++ js/app.js
@@ -1,21 +1,18 @@
 const showMap = document.getElementById('map');
-const startLocation = document.getElementById('start-location');
-const endLocation = document.getElementById('end-location');
-let latitude; 
+let latitude;
 let longitude;
 
 if (navigator.geolocation) {
   alert("Puedes usar geolocalización en tu dispositivo");
-  function initMap() {// Función para usar la longitud y latitud
+  function initMap() {
+    // Función para usar la longitud y latitud
     function localization(position) {
       // retornando longitud y latitud actual
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
       // inicio api google maps
       var uluru = { lat: latitude, lng: longitude };
-      console.log(latitude);
-      console.log(longitude);
-      var map = new google.maps.Map(document.getElementById('map'), {
+      var map = new google.maps.Map(showMap, {
         zoom: 15,
         center: uluru
       });
@@ -24,19 +21,25 @@ if (navigator.geolocation) {
         map: map
       });
       // fin api googe maps
+
     }
     function error() {
       showMap.innerHTML = '<p>No se pudo obtener tu ubicación.</p>';
     }
-    // 
-    var bla = navigator.geolocation.getCurrentPosition(localization, error);
+    navigator.geolocation.getCurrentPosition(localization, error);
 
-
-
+    // Autocompletado
+    let startLocation = document.getElementById('start-location');
+    let endLocation = document.getElementById('end-location');
+    new google.maps.places.Autocomplete(startLocation);
+    new google.maps.places.Autocomplete(endLocation);
   }
+  google.maps.event.addDomListener(window, 'load', activatePlacesSearch);
+
+  
+
 }
 else {
   alert("Lamentablemente geolocalización no funciona en tu dispositivo");
 }
-
 
