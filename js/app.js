2c1a858191ae28bf33ee10f5e53d86af94e63bf8
Danielapariona <daniela.parionac@gmail.com>
2018-02-08 01:51:35 -0500

    Muestra el nombre de la calle actual en el mapa

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let longitude;
 
 if (navigator.geolocation) {
   alert('Puedes usar geolocalización en tu dispositivo');
+
   function initMap() {
     // Función para usar la longitud y latitud
     let localization = (position) => {
@@ -11,17 +12,43 @@ if (navigator.geolocation) {
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
       // inicio api google maps
-      let getPosition = { lat: latitude, lng: longitude };
+      let getPosition = {
+        lat: latitude,
+        lng: longitude
+      };
       let map = new google.maps.Map(showMap, {
         zoom: 15,
         center: getPosition
       });
-      let marker = new google.maps.Marker({
-        position: getPosition,
-        map: map,
-        title: 'bicycle marker',
-        icon: 'assets/icon/bike.png'
-      });
+      
+      var geocoder = new google.maps.Geocoder;
+      var infowindow = new google.maps.InfoWindow;
+      geocodeLatLng(geocoder, map, infowindow);
+      
+      function geocodeLatLng(geocoder, map, infowindow) {
+        geocoder.geocode({
+          'location': getPosition
+        }, function(results, status) {
+          if (status === 'OK') {
+            if (results[1]) {
+              map.setZoom(15);
+              let marker = new google.maps.Marker({
+                position: getPosition,
+                map: map,
+                title: 'bicycle marker',
+                icon: 'assets/icon/bike.png'
+              });
+              // infowindow.setContent(results[1].formatted_address);
+              infowindow.setContent(`<div id='info_window'><strong>Geolocalización:</strong> <span id='geocodedAddress'>${results[1].formatted_address}</span><br><strong>Latitud:</strong> ${getPosition.lat.toFixed(4)} | <strong>Longitud:</strong> ${getPosition.lng.toFixed(4)}</div>`);
+              infowindow.open(map, marker);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+      }
 
       // Autocompletado
       let startLocation = document.getElementById('start-location');
@@ -50,17 +77,13 @@ if (navigator.geolocation) {
         calculateAndDisplayRoute(directionsService, directionsDisplay);
       };
       document.getElementById('mark-route').addEventListener('click', traceRoute);
-
-    }
+    };
     let error = () => {
       showMap.innerHTML = '<p>No se ingresó correctamente la dirección. Busca de nuevo.</p>';
-    }
+    };
     navigator.geolocation.getCurrentPosition(localization, error);
-
-  }
+  };
   // google.maps.event.addDomListener(window, 'load', activatePlacesSearch);
-}
-else {
+} else {
   alert('Lamentablemente geolocalización no funciona en tu dispositivo');
 }
-
