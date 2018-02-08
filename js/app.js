522d8cd4b79efce3a2fc3c5de0f1b1eaabedeab8
EstrellaBF <stern.von.nord>
2018-02-07 20:47:36 -0500

    detalles finales js y html

--- js/app.js
+++ js/app.js
@@ -6,86 +6,57 @@ if (navigator.geolocation) {
   alert("Puedes usar geolocalización en tu dispositivo");
   function initMap() {
     // Función para usar la longitud y latitud
-    function localization(position) {
+    let localization = (position) => {
       // retornando longitud y latitud actual
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
       // inicio api google maps
-      var uluru = { lat: latitude, lng: longitude };
-      var map = new google.maps.Map(showMap, {
+      let getPosition = { lat: latitude, lng: longitude };
+      let map = new google.maps.Map(showMap, {
         zoom: 15,
-        center: uluru
+        center: getPosition
       });
-      var marker = new google.maps.Marker({
-        position: uluru,
+      let marker = new google.maps.Marker({
+        position: getPosition,
         map: map
       });
-      // fin api googe maps
 
+      // Autocompletado
+      let startLocation = document.getElementById('start-location');
+      let endLocation = document.getElementById('end-location');
+      new google.maps.places.Autocomplete(startLocation);
+      new google.maps.places.Autocomplete(endLocation);
 
-    // Autocompletado
-    let startLocation = document.getElementById('start-location');
-    let endLocation = document.getElementById('end-location');
-    new google.maps.places.Autocomplete(startLocation);
-    new google.maps.places.Autocomplete(endLocation);
-
-    // Calculando ruta
-    let directionsService = new google.maps.DirectionsService;
-    let directionsDisplay = new google.maps.DirectionsRenderer;
-    let calculateAndDisplayRoute = function (directionsService, directionsDisplay) {
-      directionsService.route({
-        origin: startLocation.value,
-        destination: endLocation.value,
-        travelMode: 'DRIVING'
-      }, function(response, status){
-        if(status==='OK'){
-          directionsDisplay.setDirections(response);
-        } else {
-          window.alert('Tu ruta no se encuentra disponible');
-        }
-      });
-    };
-    directionsDisplay.setMap(map);
-    let traceRoute = ()=>{
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    document.getElementById('mark-route').addEventListener('click', traceRoute);
-
-
+      // Calculando ruta
+      let directionsService = new google.maps.DirectionsService;
+      let directionsDisplay = new google.maps.DirectionsRenderer;
+      let calculateAndDisplayRoute = (directionsService, directionsDisplay) => {
+        directionsService.route({
+          origin: startLocation.value,
+          destination: endLocation.value,
+          travelMode: 'DRIVING'
+        }, (response, status) => {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Tu ruta no se encuentra disponible');
+          }
+        });
+      };
+      directionsDisplay.setMap(map);
+      let traceRoute = () => {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+      };
+      document.getElementById('mark-route').addEventListener('click', traceRoute);
 
     }
-    function error() {
-      showMap.innerHTML = '<p>No se pudo obtener tu ubicación.</p>';
+    let error = () => {
+      showMap.innerHTML = '<p>No se ingresó correctamente la dirección. Busca de nuevo.</p>';
     }
     navigator.geolocation.getCurrentPosition(localization, error);
 
-
-    // // Función para trazar ruta
-    // let calculateAndDisplayRoute = function(directionsService, directionsDisplay){
-    //   directionsService.route({
-    //     origin: inputPartida.value,
-    //     destination: inputDestino.value,
-    //     travelMode: 'DRIVING'
-    //   }, function(response, status){
-    //     if(status==='OK'){
-    //       directionsDisplay.setDirections(response);    // Trazando ruta
-    //       directionsDisplay.setMap(map);
-    //       var setRoute = function(){
-    //         calculateAndDisplayRoute(directionsService, directionsDisplay);
-    //       };
-    //       document.getElementById('mark-route').addEventListener('click', setRoute);
-    //     } else {
-    //       window.alert('Tu ruta no se encuentra disponible');
-    //     }
-    //   });
-    // };
-
-
   }
   // google.maps.event.addDomListener(window, 'load', activatePlacesSearch);
-
-
-
 }
 else {
   alert("Lamentablemente geolocalización no funciona en tu dispositivo");
